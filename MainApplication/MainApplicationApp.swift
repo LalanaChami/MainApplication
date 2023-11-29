bcf541f692bd02af5ca5c56ec83dd396371b4861
Lalana <t.lalana@theglobalmarketaccess.com>
2023-11-29 10:47:09 +0530

    Initial Commit

---
+++ MainApplication/MainApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainApplicationApp.swift
+//  MainApplication
+//
+//  Created by Amila Manathunga on 2023-11-29.
+//
+
+import SwiftUI
+
+@main
+struct MainApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
